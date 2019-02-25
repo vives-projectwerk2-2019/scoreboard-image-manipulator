6189c6ad6293487fed2dc9d7df5ba975c7157078
Reavershark <jonasty1999@gmail.com>
2019-02-25 19:49:29 +0100

    Send image to led-display

--- source/app.d
+++ source/app.d
@@ -1,11 +1,26 @@
 import imageformats;
 import draw, config;
+import std.stdio;
+import std.file;
+
+import std.net.curl;
 
 void main()
 {
-	const config = readConfig("config.json");
+    const config = readConfig("config.json");
 
     IFImage img = read_image(config.inFile, 0);
     drawBoard(img, config);
+
     write_image(config.outFile, img.w, img.h, img.pixels);
+
+    auto buffer = read(config.outFile);
+    sendImage(buffer);
+}
+
+void sendImage(void[] data) {
+    writeln("Sending");
+    auto client = HTTP();
+    client.addRequestHeader("Content-Type", "image/png");
+    post("http://localhost:8080", data, client);
 }
